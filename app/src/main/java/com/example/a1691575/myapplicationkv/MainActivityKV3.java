9b85ae9bae257d7c0d2cad3c30e48b73dc8a141e
1691575 <karanverma2220@gmail.com>
2017-05-07 15:04:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1691575/myapplicationkv/MainActivityKV3.java
@@ -0,0 +1,13 @@
+package com.example.a1691575.myapplicationkv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityKV3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_kv3);
+    }
+}
